c46428e85a9665905976f72cbffbc7b238253860
arqam <arqam@gmail.com>
2023-04-20 19:51:53 +0500

    added with clone

---
+++ c2f.py
@@ -0,0 +1,6 @@
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert celsius to fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
---
+++ f2c.py
@@ -0,0 +1,6 @@
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Convert fahrenheit to celsius
+celsius = (fahrenheit - 32) * 5/9
+
+print(f"{fahrenheit:.2f} degrees Fahrenheit is equal to {celsius:.2f} degrees Celsius.")
